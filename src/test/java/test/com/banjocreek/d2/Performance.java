e9af3309e9f5fb2a9f37c7bc671ea2259bc5251b
Greg Wiley <aztec.rex@jammm.com>
2016-07-27 18:24:53 -0700

    profile own mutable impl

--- src/test/java/test/com/banjocreek/d2/Performance.java
+++ src/test/java/test/com/banjocreek/d2/Performance.java
@@ -7,6 +7,7 @@ import java.util.function.Supplier;
 import org.joml.Vector2d;
 
 import com.banjocreek.d2.Vec2;
+import com.banjocreek.d2m.Vec2m;
 
 public final class Performance {
 
@@ -31,6 +32,16 @@ public final class Performance {
         
     }
     
+    public static Vec2m mutable2() {
+        Vec2m v = new Vec2m(1, 1);
+        Vec2m increment = new Vec2m(-.001,.001);
+        for( long i=0; i < iterations; ++i) {
+            v.plus(increment);
+        }
+        return v;
+        
+    }
+
     private static <R> Duration profile (String tag, Supplier<R> f) {
         
         Instant start = Instant.now();
@@ -46,17 +57,22 @@ public final class Performance {
         mutable();      // warm it up
         Duration mp = profile("mutable", Performance::mutable);
         
-        immutable();    // warm it up
-        Duration ip = profile("immutable",Performance::immutable);
+        mutable2();    // warm it up
+        Duration mp2 = profile("mutable2",Performance::mutable2);
 
+        immutable(); // warm it up
+        Duration ip = profile("immutable", Performance::immutable);
+        
         float compare = (float)ip.toMillis() / (float)mp.toMillis();
         
         System.out.println("iduration/mduration: " + compare);
         
         System.out.println("mutable ops/s: " + ((double)iterations/(double)mp.toMillis()));
+        System.out.println("mutable2 ops/s: " + ((double)iterations/(double)mp2.toMillis()));
         System.out.println("immutable ops/s: " + ((double)iterations/(double)ip.toMillis()));
         
         System.out.println("mutable ops/f: " + ((double)iterations/(double)mp.toMillis()/50d));
+        System.out.println("mutable2 ops/f: " + ((double)iterations/(double)mp2.toMillis()/50d));
         System.out.println("immutable ops/f: " + ((double)iterations/(double)ip.toMillis()/50d));
 
     
