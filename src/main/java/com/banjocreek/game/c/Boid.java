de5f562f8a5678954bdff9ccf9faaece3ecaa746
Greg Wiley <aztec.rex@jammm.com>
2016-08-02 19:35:08 -0700

    boid uses new vectors

--- src/main/java/com/banjocreek/game/c/Boid.java
+++ src/main/java/com/banjocreek/game/c/Boid.java
@@ -3,122 +3,119 @@ package com.banjocreek.game.c;
 import java.awt.geom.Point2D;
 import java.util.Random;
 
-import org.joml.Vector2d;
+import com.banjocreek.d2.MutableVec2;
+import com.banjocreek.d2.Vec2;
 
 public final class Boid implements GameObject {
 
-	private static final double inverseMass = 1d / 1d;
-	private static final double maxForce = 2d;
+    private static final double inverseMass = 1d / 1d;
+    private static final double maxForce = 2d;
 
-	private static final double maxSpeed = 2d; // meters per second
+    private static final double maxSpeed = 2d; // meters per second
 
-	private static final double slowRadius = 3d;
+    private static final Random rng = new Random();
 
-	private static final Random rng = new Random();
-	
-	// destructive
-	private static Vector2d truncate(Vector2d v, double max) {
-		if (v.length() > max) {
-			v.normalize().mul(max);
-		}
-		return v;
-	}
+    private static final double slowRadius = 3d;
 
-	private final Point2D ppos = new Point2D.Double();
-	private double rotation = 0d;
-	private final Vector2d position = new Vector2d();
-	private final Vector2d velocity = new Vector2d();
-	private final Vector2d target = new Vector2d();
-	private final Vector2d danger = new Vector2d();
-	private final Vector2d scratch = new Vector2d();
-	private final Vector2d steering = new Vector2d();
+    private final MutableVec2 danger = Vec2.mutable();
+    private final MutableVec2 position = Vec2.mutable();
 
-	private void seek() {
-		scratch.set(target).sub(position); 	// vector to target
-		double distance = scratch.length();	// distance to target
-		if (distance == 0d)
-			return; 						// no influence
-		scratch.normalize().mul(maxSpeed); 	// desired velocity
-		if (distance < slowRadius) {
-			scratch.mul(distance / slowRadius); // nearing target, slow down
-		}
-		scratch.sub(velocity); 				// steering force a la Reynolds
-		steering.add(scratch); 				// accumulate force
-	}
+    private final Point2D ppos = new Point2D.Double();
+    private double rotation = 0d;
+    private final MutableVec2 scratch = Vec2.mutable();
+    private final MutableVec2 steering = Vec2.mutable();
+    private final MutableVec2 target = Vec2.mutable();
+    private final MutableVec2 velocity = Vec2.mutable();
 
-	private void avoid() {
-		scratch.set(danger).sub(position);	// vector to danger
-		double distance = scratch.length();
-		if (distance > 1d)
-			return;							// no influence
-		if (distance == 0d)
-			scratch.set(rng.nextDouble(),rng.nextDouble());	// danger right on us
-		
-		scratch.normalize().mul(-maxSpeed); // desired velocity
-		scratch.sub(velocity); 				// steering force a la Reynolds
-		steering.add(scratch);				// accumulate force
+    public Boid(final Point2D p0) {
+        this.position.set(p0.getX(), p0.getY());
+        this.target.set(this.position);
+        this.danger.set(500, 500).plus(this.position);
+        this.velocity.zero();
+        publish();
+    }
 
-	}
+    public Boid avoid(final Point2D target) {
+        this.danger.set(target.getX(), target.getY());
+        return this;
+    }
 
-	private void applyForce(double dt) {
-		truncate(steering, maxForce);  // no warp drive
-		scratch.set(steering).mul(inverseMass);	// convert to acceleration
-		velocity.add(scratch.mul(dt)); // thanks, Leonhard
-	}
-	
-	private void applyVelocity(double dt) {
-		truncate(velocity, maxSpeed);
-		if (velocity.length() < .01)
-			return;					// static friction, keep things from bouncing around
-		scratch.set(velocity);
-		position.add(scratch.mul(dt)); // integrate
-	}
-	
-	private void publish() {
-		rotation = Math.atan2(velocity.y, velocity.x);
-		ppos.setLocation(position.x, position.y);
-		
-	}
-	
-	@Override
-	public void update(double dt) {
-		steering.zero(); // re-compute all steering forces
-		seek(); // seek force
-		avoid(); // run away
+    @Override
+    public Point2D position() {
+        return this.ppos;
+    }
 
-		applyForce(dt);
-		applyVelocity(dt);
+    @Override
+    public double rotation() {
+        return this.rotation;
+    }
 
+    public Boid seek(final Point2D target) {
+        this.target.set(target.getX(), target.getY());
+        return this;
+    }
 
-		publish();
-	}
+    @Override
+    public void update(final double dt) {
+        this.steering.zero(); // re-compute all steering forces
+        seek(); // seek force
+        avoid(); // run away
 
-	public Boid(Point2D p0) {
-		this.position.set(p0.getX(), p0.getY());
-		this.target.set(this.position);
-		this.danger.set(500, 500).add(this.position);
-		this.velocity.zero();
-		publish();
-	}
+        applyForce(dt);
+        applyVelocity(dt);
 
-	@Override
-	public Point2D position() {
-		return ppos;
-	}
+        publish();
+    }
 
-	@Override
-	public double rotation() {
-		return rotation;
-	}
+    private void applyForce(final double dt) {
+        this.steering.truncate(maxForce); // no warp drive
+        this.scratch.set(this.steering).scale(inverseMass); // convert to
+                                                            // acceleration
+        this.velocity.plus(this.scratch.scale(dt)); // thanks Leonhard
+    }
 
-	public Boid seek(Point2D target) {
-		this.target.set(target.getX(), target.getY());
-		return this;
-	}
+    private void applyVelocity(final double dt) {
+        this.velocity.truncate(maxSpeed);
+        if (this.velocity.magnitude() < .01)
+            return; // static friction, keep things from bouncing around
+        this.scratch.set(this.velocity);
+        this.position.plus(this.scratch.scale(dt)); // integrate
+    }
 
-	public Boid avoid(Point2D target) {
-		this.danger.set(target.getX(), target.getY());
-		return this;
-	}
+    private void avoid() {
+        this.scratch.set(this.danger).minus(this.position); // vector to danger
+        final double distance = this.scratch.magnitude();
+        if (distance > 1d)
+            return; // no influence
+        if (distance == 0d) {
+            this.scratch.set(rng.nextDouble(), rng.nextDouble()); // danger
+                                                                  // right on us
+        }
+
+        this.scratch.normalize().scale(-maxSpeed); // desired velocity
+        this.scratch.minus(this.velocity); // steering force a la Reynolds
+        this.steering.plus(this.scratch); // accumulate force
+
+    }
+
+    private void publish() {
+        this.rotation = Math.atan2(this.velocity.y(), this.velocity.x());
+        this.ppos.setLocation(this.position.x(), this.position.y());
+
+    }
+
+    private void seek() {
+        this.scratch.set(this.target).minus(this.position); // vector to target
+        final double distance = this.scratch.magnitude(); // distance to target
+        if (distance == 0d)
+            return; // no influence
+        this.scratch.normalize().scale(maxSpeed); // desired velocity
+        if (distance < slowRadius) {
+            this.scratch.scale(distance / slowRadius); // nearing target, slow
+                                                       // down
+        }
+        this.scratch.minus(this.velocity); // steering force a la Reynolds
+        this.steering.plus(this.scratch); // accumulate force
+    }
 
 }
